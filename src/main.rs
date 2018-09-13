99e2853070755e86b83c8c1ccf40d098aee1f329
Juha Komulainen <juha.komulainen@evident.fi>
2018-09-13 09:48:57 +0300

    Write png images

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 extern crate cgmath;
+extern crate image;
 
 mod ray;
 
-use std::io::BufWriter;
-use std::io::prelude::*;
-use std::fs::File;
 use cgmath::prelude::*;
 use cgmath::{dot, vec3, Vector3};
 use ray::Ray;
+use image::ImageBuffer;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let nx = 200;
     let ny = 100;
 
@@ -18,32 +17,28 @@ fn main() -> std::io::Result<()> {
     let vertical = vec3(0.0, 2.0, 0.0);
     let origin = vec3(0.0, 0.0, 0.0);
 
-    let file = File::create("images/bar.ppm")?;
-    let ref mut ppm = BufWriter::new(file);
+    let img = ImageBuffer::from_fn(nx, ny, |i, j| {
+        let j = ny - j;
+        let u = (i as f64) / (nx as f64);
+        let v = (j as f64) / (ny as f64);
 
-    ppm.write_fmt(format_args!("P3\n{} {}\n255\n", nx, ny))?;
-    for j in (0..ny).rev() {
-        for i in 0..nx {
-            let u = (i as f64) / (nx as f64);
-            let v = (j as f64) / (ny as f64);
+        let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+        let c = color(&r);
+        let ir = (255.99 * c.x) as u8;
+        let ig = (255.99 * c.y) as u8;
+        let ib = (255.99 * c.z) as u8;
 
-            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
-            let c = color(&r);
-            let ir = (255.99 * c.x) as i32;
-            let ig = (255.99 * c.y) as i32;
-            let ib = (255.99 * c.z) as i32;
+        image::Rgb([ir, ig, ib])
+    });
 
-            ppm.write_fmt(format_args!("{} {} {}\n", ir, ig, ib))?;
-        }
-    }
-
-    Ok(())
+    img.save("images/foo.png").unwrap();
 }
 
 fn color(r: &Ray) -> Vector3<f64> {
     if hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r) {
         return vec3(1.0, 0.0, 0.0);
     }
+
     let unit_direction = r.direction.normalize();
     let t = 0.5 * (unit_direction.y + 1.0);
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
